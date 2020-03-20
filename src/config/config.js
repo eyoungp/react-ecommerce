97583ffe088ce03714849e530f16bfaa570c0f4c
EC2 Default User <ec2-user@ip-172-31-46-109.ap-northeast-1.compute.internal>
2020-03-20 06:04:15 +0000

    https://3owli2gq80.execute-api.ap-northeast-2.amazonaws.com/default/event_handler

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-const API_SERVER = ''
+const API_SERVER = 'http://13.125.79.26:5000'
 
 const CONFIG = {
-  EVENT_REGISTER: `${API_SERVER}/evetns/register`,
+  EVENT_REGISTER: `https://3owli2gq80.execute-api.ap-northeast-2.amazonaws.com/default/event_handler`,
   USER_LOGIN: `${API_SERVER}/users/authenticate`,
   USER_SIGNUP: `${API_SERVER}/users/register`,
   COLLECTION: `${API_SERVER}/collection`
