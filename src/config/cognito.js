bcd06b23dc07e8521c7b1ab819fe4c448f9d5da8
EC2 Default User <ec2-user@ip-172-31-46-109.ap-northeast-1.compute.internal>
2020-03-20 06:58:49 +0000

    https://3owli2gq80.execute-api.ap-northeast-2.amazonaws.com/default/event_handler

--- src/config/cognito.js
+++ src/config/cognito.js
@@ -1,6 +1,6 @@
 export default {
   cognito: {
-    REGION: "ap-northeast",
+    REGION: "ap-northeast-2",
     USER_POOL_ID: "ap-northeast-2_tVxIvynZ9",
     APP_CLIENT_ID: "15h4h90epbu94884rt97aomd33",
     IDENTITY_POOL_ID: "ap-northeast-2:93f11fbf-6f70-4ba8-90e3-3a5a5a774cd0"
